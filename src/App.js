dd37f33efa581f2213d7eb242e0ea9a643979f1f
rreznichenko <demezis1994@gmal.com>
2019-07-03 02:59:42 +0300

    added menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import Header from './components/Header';
+import NavMenuDrawer from './components/NavMenuDrawer';
 
 const theme = createMuiTheme()
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <Header />
+        <Header setIsMenuOpen={setIsMenuOpen} />
+        <NavMenuDrawer isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       </ThemeProvider>
     </div>
   );
