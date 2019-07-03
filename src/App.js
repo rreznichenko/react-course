29ff95ef7fdab0691fce910e09e068d96468b0b6
rreznichenko <demezis1994@gmal.com>
2019-07-04 02:43:38 +0300

    added basic router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
+
 import Header from './components/Header';
 import NavMenuDrawer from './components/NavMenuDrawer';
+
+import Main from './containers/Main';
+import Teams from './containers/Teams';
+import Team from './containers/Team';
+import Page404 from './containers/Page404';
 
 const theme = createMuiTheme()
 
@@ -13,6 +20,14 @@ function App() {
       <ThemeProvider theme={theme}>
         <Header setIsMenuOpen={setIsMenuOpen} />
         <NavMenuDrawer isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/teams" component={Teams} />
+            <Route exact path="/team/:id" component={Team} />
+            <Route component={Page404} />
+          </Switch>
+        </Router>
       </ThemeProvider>
     </div>
   );
