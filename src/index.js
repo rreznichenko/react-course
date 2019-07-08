b99163988e3e53a33eeb66bf993b18daf159064c
rreznichenko <demezis1994@gmal.com>
2019-07-09 01:51:52 +0300

    store setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render((<Router><App /></Router>), document.getElementById('root'));
+import mainReducer from './containers/Main/reducer';
+import teamReducer from './containers/Team/reducer';
+import teamsReducer from './containers/Teams/reducer';
+
+const rootReducer = combineReducers({
+    main: mainReducer,
+    teams: teamsReducer,
+    team: teamReducer
+})
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render((
+                <Provider store={store}>    
+                    <Router>
+                        <App />
+                    </Router>
+                </Provider>), document.getElementById('root'));
 
